772ac6cefc8f8792c02ec19eeec6e7969de6d70e
MaxiSch <sevenap@gmail.com>
2018-02-20 13:23:02 -0300

    MutantController update and sbt duplicate removal

--- mutants/app/controllers/MutantController.java
+++ mutants/app/controllers/MutantController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import models.Dna;
+import play.data.Form;
+import play.data.FormFactory;
 import play.libs.Json;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
@@ -8,18 +11,36 @@ import services.MutantService;
 import javax.inject.Inject;
 import java.util.concurrent.CompletionStage;
 
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
 public class MutantController extends Controller {
 
     private final MutantService mutantService;
     private final HttpExecutionContext httpExecutionContext;
+    private final FormFactory formFactory;
 
     @Inject
-    public MutantController(MutantService mutantService, HttpExecutionContext httpExecutionContext) {
+    public MutantController(MutantService mutantService,
+                            HttpExecutionContext httpExecutionContext,
+                            FormFactory formFactory) {
         this.mutantService = mutantService;
         this.httpExecutionContext = httpExecutionContext;
+        this.formFactory = formFactory;
     }
 
     public CompletionStage<Result> isMutant() {
+        Form<Dna> dnaForm = formFactory.form(Dna.class).bindFromRequest();
+
+        if (dnaForm.hasErrors()) {
+            return supplyAsync(() -> {
+                return badRequest();
+            }, httpExecutionContext.current());
+        }
+
+        Dna dna = dnaForm.get();
+
+        System.out.println(dna);
+
         return this.mutantService.isMutant(null).thenApplyAsync(mutantResult -> {
             return ok(mutantResult.toString());
         }, httpExecutionContext.current());
--- mutants/build.sbt
+++ mutants/build.sbt
@@ -5,8 +5,6 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
-libraryDependencies += guice
-
 scalaVersion := "2.12.4"
 
 libraryDependencies += guice
