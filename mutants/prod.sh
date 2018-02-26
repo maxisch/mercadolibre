f11825219f8f4eea143c798c331508791e469810
MaxiSch <sevenap@gmail.com>
2018-02-25 21:16:56 -0300

    added filters and production script

---
+++ mutants/prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup ./bin/mutants -Dhttp.port=9000 -DapplyEvolutions.default=true -Dconfig.file=conf/application.conf -Dplay.http.secret.key='nzG]px^Hd^^vXmjHS:/6WAkcsc0f2oJxmNA7k>?q9tWc?td^]4Q8]w`o@BmBl4Fb' &
